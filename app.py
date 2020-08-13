795b8f2c3f3b840efc74cf00df244f35dca2d872
Cheban Alexei <alexei.cheban@syntech.software>
2020-08-13 11:17:15 +0300

    add cors

--- app.py
+++ app.py
@@ -4,8 +4,18 @@ from uuid import uuid4
 import aiofiles
 import boto3
 import fastapi
+from fastapi.middleware.cors import CORSMiddleware
 
 app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 meeting_tables = {}
 
